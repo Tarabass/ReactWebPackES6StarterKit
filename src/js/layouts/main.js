847c646cb584684d1ec8bac80c70dab21449aa8c
Tarabass <p.rietveld@live.com>
2016-04-27 21:14:29 +0200

    Use the native map function to iterate over an array of links from within JSX

--- src/js/layouts/main.js
+++ src/js/layouts/main.js
@@ -26,14 +26,23 @@ const Main = React.createClass({
 		};
 	},
 	render: function() {
+		const links = [{
+				text: 'Home',
+				href: '/'
+			}, {
+				text: 'Users',
+				href: '/users'
+			}, {
+				text: 'Widgets',
+				href: 'widgets'
+			}];
+
 		return (
 			<div className="app">
 				<header className="primary-header"></header>
 				<aside className="primary-aside">
 					<ul>
-						<li><Link to="/" activeClassName="active">Home</Link></li>
-						<li><Link to="/users" activeClassName="active">Users</Link></li>
-						<li><Link to="widgets" activeClassName="active">Widgets</Link></li>
+						{links.map(link => <li><Link to={link.href} activeClassName="active">{link.text}</Link></li>)}
 					</ul>
 				</aside>
 				<div onClick={this.handleClick}>handle click of createClass instance (main.js)</div>
