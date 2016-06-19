74f95b89130a11fbf1276ed8cbc6adc1ec44891b
Tarabass <p.rietveld@live.com>
2016-06-19 22:49:02 +0200

    Add paragraph with link to react-es5-createclass-vs-es6-classes article

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -36,6 +36,7 @@ class Home extends Component {
 				<Paragraph text="I followed a tutorial on:" />
 				<Paragraph text="http://edspencer.net/2016/03/20/a-new-stack-for-2016-getting-started-with-react-es6-and-webpack/" />
 				<Paragraph text="https://css-tricks.com/learning-react-router/" />
+				<Paragraph text="https://daveceddia.com/react-es5-createclass-vs-es6-classes/" />
 				<LinkButton color="green" text="I also read about React.createClass versus extends Component" link="https://toddmotto.com/react-create-class-versus-component/" />
 				<LinkButton text="Using css/sass is talked about here" link="https://www.bensmithett.com/smarter-css-builds-with-webpack/" />
 				<div onClick={this._handleClick.bind(this)}>handle click of extend Components (home.js)</div>
